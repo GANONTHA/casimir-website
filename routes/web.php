c5714ffb2a78ea52e442d822511528b6676daa8d
Bruno <bganontha@gmail.com>
2024-04-01 15:19:18 +0100

    added route to blog

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\templateController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [templateController::class, 'index']);
+Route::get('/blogs', [BlogController::class, 'index']);
