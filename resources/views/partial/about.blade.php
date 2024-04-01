91ca4580d75709bd19b1cec9d7ce9967c263915c
Bruno <bganontha@gmail.com>
2024-04-01 15:58:49 +0100

    Add call to action section, navigation bar, stats section, and hero section to views

---
+++ resources/views/partial/about.blade.php
@@ -0,0 +1,63 @@
+<section class="ftco-about ftco-section ftco-no-pt ftco-no-pb" id="about-section">
+		<div class="container">
+			<div class="row d-flex no-gutters">
+				<div class="col-md-6 col-lg-5 d-flex">
+					<div class="img-about img d-flex align-items-stretch">
+						<div class="overlay"></div>
+						<div class="img d-flex align-self-stretch align-items-center" style="background-image:url(images/about-01.jpg);">
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-7 pl-md-4 pl-lg-5 py-5">
+					<div class="py-md-5">
+						<div class="row justify-content-start pb-3">
+							<div class="col-md-12 heading-section ftco-animate">
+								<span class="subheading">Mon Intro</span>
+								<h2 class="mb-4" style="font-size: 34px; text-transform: capitalize;">A Propos de moi</h2>
+								<p>Je suis Doctorant en Physique et Science de l’ingénieur : option <span>Energies Renouvelables</span> <br>
+								<i><< Qui ose, Grandit, qui grandit, Ose ! >> </i>
+								</p>
+
+								<ul class="about-info mt-4 px-md-0 px-2">
+									<li class="d-flex"><span>Nom:</span> <span>Casimir Fanezoune Kalibe</span></li>
+									{{-- <li class="d-flex"><span>Date of birth:</span> <span>January 01, 1990</span></li> --}}
+									<li class="d-flex"><span>Adresse:</span> <span>San Francisco CA 97987 USA</span></li>
+									<li class="d-flex"><span>Zip code:</span> <span>1000</span></li>
+									<li class="d-flex"><span>Email:</span> <span>kalibefanezounecasimir4@gmail.com</span></li>
+									<li class="d-flex"><span>Téléphone: </span> <span>+212 684-966602</span></li>
+								</ul>
+							</div>
+							<div class="col-md-12">
+								<div class="my-interest d-lg-flex w-100">
+									<div class="interest-wrap d-flex align-items-center">
+										<div class="icon d-flex align-items-center justify-content-center">
+											<span class="flaticon-listening"></span>
+										</div>
+										<div class="text">Musique</div>
+									</div>
+									<div class="interest-wrap d-flex align-items-center">
+										<div class="icon d-flex align-items-center justify-content-center">
+											<span class="flaticon-suitcases"></span>
+										</div>
+										<div class="text">Voyage</div>
+									</div>
+									<div class="interest-wrap d-flex align-items-center">
+										<div class="icon d-flex align-items-center justify-content-center">
+											<span class="flaticon-video-player"></span>
+										</div>
+										<div class="text">Cinéma</div>
+									</div>
+									<div class="interest-wrap d-flex align-items-center">
+										<div class="icon d-flex align-items-center justify-content-center">
+											<span class="flaticon-football"></span>
+										</div>
+										<div class="text">Sports</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
---
+++ resources/views/partial/hero.blade.php
@@ -0,0 +1,42 @@
+<section id="home-section" class="hero">
+		<div class="home-slider owl-carousel">
+			<div class="slider-item">
+				<div class="overlay"></div>
+				<div class="container-fluid px-md-0">
+					<div class="row d-md-flex no-gutters slider-text align-items-end justify-content-end" data-scrollax-parent="true">
+						<div class="one-third order-md-last img" style="background-image:url(images/bg_1_old-rem.png);">
+							<div class="overlay"></div>
+							<div class="overlay-1"></div>
+						</div>
+						<div class="one-forth d-flex  align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+							<div class="text">
+								<span class="subheading">Salut, c'est Casimir</span>
+								
+								<h1 class="mb-4 mt-3">Doctorant en <span>Physique et Science</span> de l’ingénieur</h1>
+								<p><a href="#" class="btn btn-primary">Me Contacter</a> <a href="#" class="btn btn-primary btn-outline-primary">Voir CV</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="slider-item">
+				<div class="overlay"></div>
+				<div class="container-fluid px-md-0">
+					<div class="row d-flex no-gutters slider-text align-items-end justify-content-end" data-scrollax-parent="true">
+						<div class="one-third order-md-last img" style="background-image:url(images/bg_2.png);">
+							<div class="overlay"></div>
+							<div class="overlay-1"></div>
+						</div>
+						<div class="one-forth d-flex align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+							<div class="text">
+								<span class="subheading">Energies Renouvelables</span>
+								<h1 class="mb-4 mt-3">Je suis <span>Casimir KF</span> <br> Bienvenu sur mon site  personnel.</h1>
+								<p><a href="#" class="btn btn-primary">Me Contacter</a> <a href="#" class="btn btn-primary btn-outline-primary">Voir CV</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
