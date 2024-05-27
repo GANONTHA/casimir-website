63e100548aefc29ed332e4c8339faec9795b045d
Bruno <bganontha@gmail.com>
2024-05-27 12:52:26 +0100

    footer

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -21,7 +21,7 @@
 						</ul>
 					</div>
 				</div>
-				<div class="col-md">
+				{{-- <div class="col-md">
 					<div class="ftco-footer-widget mb-4">
 						<h2 class="ftco-heading-2">Services</h2>
 						<ul class="list-unstyled">
@@ -32,14 +32,14 @@
 							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Graphic Design</a></li>
 						</ul>
 					</div>
-				</div>
+				</div> --}}
 				<div class="col-md">
 					<div class="ftco-footer-widget mb-4">
 						<h2 class="ftco-heading-2">Avez-vous une Question?</h2>
 						<div class="block-23 mb-3">
 							<ul>
-								<li><span class="icon fa fa-map marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-								<li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+								<li><span class="icon fa fa-map marker"></span><span class="text">Maroc</span></li>
+								<li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+212 684-966602</span></a></li>
 								<li><a href="kalibefanezounecasimir4@gmail.com"><span class="icon fa fa-paper-plane pr-4"></span><span class="text">kalibefanezounecasimir4@gmail.com</span></a></li>
 							</ul>
 						</div>
